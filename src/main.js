2fb25c407d8a5a15f72ccbb5562df6c866bc6723
dariamorozova-dev <mdash.tv@gmail.com>
2020-07-19 18:11:23 +0200

    00

--- src/main.js
+++ src/main.js
@@ -1,19 +1,53 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},    
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];//массив товаров с версткой
+        this._fetchProducts();
+        this.render();//вывод товаров на страницу
+    }
 
-const renderProduct = ({id= 00, title = 'Product_#', price = 1000}) => {
-    return `<div class="product-item">
-                <h3 class="product-item__title">${title}</h3>
-                <img src="../img/${id}.jpg" alt="Photo" height="115" class="product-item__img">
-                <p class="product-item__price">${price} монет</p>
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            this.allProducts.push(item);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product) {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = `../img/${this.id}.jpg`;
+    }
+
+    render() {
+        return `<div class="product-item">
+                <h3 class="product-item__title">${this.title}</h3>
+                <img src="${this.img}" alt="Photo" height="115" class="product-item__img"">
+                <p class="product-item__price">${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
+    }
+}
 
-const renderPage = list => document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join(' ');
+class Cart {
+    constructor() {
+        this.cartBtn = document.querySelector('.btn-cart');
+    }
+}
 
-renderPage(products);
+let list = new ProductList();
