38a77479e9619595944d0443a3fafae0871eb3d4
dariamorozova-dev <mdash.tv@gmail.com>
2020-08-06 14:17:37 +0200

    init

--- Hamburger/src/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-    const display = new Display();
-    const hamburger = new Hamburger();
-
-    hamburger.init(display);
-    display.init(hamburger);
-})
--- my_vers/src/main.js
+++
@@ -1,9 +0,0 @@
-
-
-window.addEventListener('load', () => {
-    const list = new ProductList();
-    const cart = new Cart();
-    console.log(list.calcSumOfAllProducts());
-    list.init(cart);
-    console.log(cart);
-})
