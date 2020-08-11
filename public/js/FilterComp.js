c8c9fd7af565ff8ae40104574d63e2fd78a68612
dariamorozova-dev <mdash.tv@gmail.com>
2020-08-11 14:06:12 +0200

    filter comp

--- public/js/FilterComp.js
+++ public/js/FilterComp.js
@@ -1,16 +1,20 @@
-const FilterComp = Vue.component('filter-el', {
-    data(){
-      return {
-          userSearch: ''
-      }
-    },
-    template: `<form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
-                <input type="text" class="search-field" v-model="userSearch">
-                <button type="submit" class="btn-search">
-                    <i class="fas fa-search"></i>
-                </button>
-            </form>`
-});
+import filter from './FilterComp.vue';
+Vue.component('.filter-el', filter);
+
+
+// const FilterComp = Vue.component('filter-el', {
+//     data(){
+//       return {
+//           userSearch: ''
+//       }
+//     },
+//     template: `<form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
+//                 <input type="text" class="search-field" v-model="userSearch">
+//                 <button type="submit" class="btn-search">
+//                     <i class="fas fa-search"></i>
+//                 </button>
+//             </form>`
+// });
 
 // export default {
 //   Filter: FilterComp,
